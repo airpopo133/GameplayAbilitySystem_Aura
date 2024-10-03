ceb4c35bde921d5c313292342cef29a67429d42e
unknown <airpopo133@gmail.com>
2024-10-03 13:10:15 +0800

    Player and Enemy Class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,22 @@
+// Copyright Druid Mechanics
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false
+	;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
